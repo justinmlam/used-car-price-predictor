cd36039c5b4ad67f9ea27f225975816510ce8d25
justinmlam <justinmlam@gmail.com>
2020-05-24 12:48:32 -0700

    Added base components on Front End styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import CarForm from './CarForm'
+import styled from 'styled-components';
+
+const Heading = styled.div`
+  text-align: center;
+  margin: 18rem auto 10rem;
+  display: block;
+  font-size: 25px;
+
+`
+
+const Main = styled.div`
+  text-align: center;
+`
 
 function App() {
   return (
     <>
-      Car form
-      <CarForm />
+      <Heading>Car Form</Heading>
+      <Main><CarForm /></Main>
     </>
   )
 }
--- src/CarForm.js
+++ src/CarForm.js
@@ -1,4 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+    color: red;
+    text-align: center;
+`;
+
+const Field = styled.div`
+    float:left;
+    width:33%;
+`;
+
+const Label = styled.label`
+    text-align: center;
+`;
+
+const Input = styled.input`
+    text-align: center;
+`;
 
 class CarForm extends Component {
 
@@ -37,16 +56,22 @@ class CarForm extends Component {
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <label>Brand</label>
-                <input type='text' value={this.state.brand} onChange={this.handleBrandChange} />
+                <Field>
+                    <Label>Brand</Label><br />
+                    <Input type='text' value={this.state.brand} onChange={this.handleBrandChange} />
+                </Field>
 
-                <label>Model</label>
-                <input type='text' value={this.state.model} onChange={this.handleModelChange} />
+                <Field>
+                    <Label>Model</Label><br />
+                    <Input type='text' value={this.state.model} onChange={this.handleModelChange} />
+                </Field>
+               
+               <Field>
+                   <Label>Year</Label><br />
+                   <Input type='text' value={this.state.year} onChange={this.handleYearChange} />
+               </Field>
 
-                <label>Year</label>
-                <input type='text' value={this.state.year} onChange={this.handleYearChange} />
-
-                <button type='submit'>Submit</button>
+                <Button type='submit'>Submit</Button> 
             </form>
         )
     }
