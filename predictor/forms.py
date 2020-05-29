973888353241599477e66de95a873f6e41b47157
justinmlam <justinmlam@gmail.com>
2020-05-29 08:03:25 -0700

    Added all fields for predictor form

--- predictor/forms.py
+++ predictor/forms.py
@@ -10,3 +10,20 @@ class VehicleForm(forms.Form):
     year = forms.CharField(max_length=4,
                            label="Year",
                            widget=forms.TextInput(attrs={'class': 'text-field'}))
+
+    odometer = forms.CharField(max_length=100,
+                            label="Odometer",
+                            widget=forms.TextInput(attrs={'class': 'text-field'}))
+    title = forms.CharField(max_length=100,
+                            label="Title Status",
+                            widget=forms.TextInput(attrs={'class': 'text-field'}))
+
+    color = forms.CharField(max_length=100,
+                            label="Colour",
+                            widget=forms.TextInput(attrs={'class': 'text-field'}))
+    transmission = forms.CharField(max_length=100,
+                            label="Transmission",
+                            widget=forms.TextInput(attrs={'class': 'text-field'}))
+    condition = forms.CharField(max_length=100,
+                            label="Condition",
+                            widget=forms.TextInput(attrs={'class': 'text-field'}))
